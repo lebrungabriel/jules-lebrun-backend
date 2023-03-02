7c87abc71ed80a9e3cea0859eed14dfa32cea3b1
Gabriel Lebrun <lebrungabriel75@gmail.com>
2023-03-02 09:26:13 +0100

    vercel.json modified

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ var usersRouter = require("./routes/users");
 var app = express();
 
 const cors = require("cors");
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(fileUpload());
 
